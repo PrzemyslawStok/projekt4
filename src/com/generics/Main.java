8520856b3ea31d495dfb5633761221bb9f3c6e85
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-30 13:12:01 +0100

    dodano definicje

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -1,9 +1,12 @@
 package com.generics;
 
 public class Main {
+    double a = 0;
+    double b = 1;
+    double c = 2;
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("napis");
     }
 }
