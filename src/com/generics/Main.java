301a0349bd9cac5078af0ed24633f7f845d60485
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-11-30 15:01:49 +0100

    dodano prostokat

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -22,8 +22,18 @@ public class Main {
         wzrost = scanner.nextInt();
     }
 
+    public static void figura(){
+        
+        for(int j=0;j<10;j++) {
+            for (int i = 0; i < 10; i++)
+                System.out.print("*");
+
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
-        // write your code here
-        wyswietl();
+        //wyswietl();
+        figura();
     }
 }
